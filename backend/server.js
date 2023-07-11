dba61d15d484af15e9eb2aaa333bd1e72026033c
Vikas Soni <sonivikas142@gmail.com>
2023-07-11 11:20:55 +0530

    fix for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,15 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
+const port  = process.env.PORT || 4000;
 
-const  PORT = 4000; 
-
-//config...
-dotenv.config({ path: "backend/config/config.env" });
+// //config...
+// dotenv.config({ path: "backend/config/config.env" });
 
 //Connecting to database
 connectDatabase();
 
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+app.listen(port,()=>{
+    console.log(`Server is running at ${port}`);
 })  
